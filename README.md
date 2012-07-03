0175874d4d7e632291cae28012ee7d6c74ef013e
Anurag Panda <Anurag@fosspowered.com>
2012-07-04 02:00:55 +0530

    Fixed up whitespace.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 BlackJack
 =========
 
-Black Jack game made using TDD and SOLID Principles.
+Black Jack game made using TDD and SOLID Principles using Ruby.
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,7 @@
 class Deck
   attr_reader :deck
   def initialize
-  	suit = ['D','H','C','S']
+    suit = ['D','H','C','S']
     rank = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
 
     @deck = suit.map { |x| rank.map { |y| "#{x}#{y}" }}.flatten # Create a rank suit pair representing every card in the deck. "DA", "S10", "H4", etc.

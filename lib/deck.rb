70044426875531c1eda4614bcb75c6d6529a1d25
Smit Shah <who828@gmail.com>
2012-07-04 01:46:55 +0530

    Added variables to Deck initalization

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,9 @@
 class Deck
   attr_reader :deck
   def initialize
-    @deck = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'].map { |x| ['D','H','C','S'].map { |y| "#{x}#{y}" }}.flatten
+    rank = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+    suit = ['D','H','C','S']
+    @deck = rank.map { |x| suit.map { |y| "#{x}#{y}" }}.flatten
   end
 end
 
